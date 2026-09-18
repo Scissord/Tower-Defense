c46c3b3f92b593c18c3651304282d4449612fac5
scissxrd <scissxrd@gmail.com>
2026-09-18 23:03:04 +0500

    completed course

---
+++ Assets/Scripts/SpeedUpManager.cs
@@ -0,0 +1,26 @@
+using UnityEngine;
+using UnityEngine.UI;
+
+public class SpeedUpManager : MonoBehaviour
+{
+    public bool speedUp;
+    public Image buttonImage;
+    public Color normalColor;
+    public Color speedUpColor;
+
+    public void ToggleSpeedUp()
+    {
+        speedUp = !speedUp;
+
+        if (speedUp)
+        {
+            Time.timeScale = 2;
+            buttonImage.color = speedUpColor;
+        }
+        else
+        {
+            Time.timeScale = 1;
+            buttonImage.color = normalColor;
+        }
+    }
+}
